e99b8dc703a8fbb701c482cd1933dfdf985198ec
Alexandre Guerra <guerra@wdev.int>
2019-12-12 17:13:58 -0300

    details and pagination

--- django/core/views.py
+++ django/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from core.models import Movie
 
-# Create your views here.
+
+class MovieList(ListView):
+	model = Movie
+
+
+class MovieDetail(DetailView):
+	model = Movie
+
