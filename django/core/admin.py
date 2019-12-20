6a718bf6f09b47749d38b7a6fff0b65526ebc4bc
Alexandre Guerra <guerra@wdev.int>
2019-12-20 17:28:15 -0300

    ajustes

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from core.models import Movie
+from core.models import Movie, Person, Role
 
 admin.site.register(Movie)
+admin.site.register(Person)
+admin.site.register(Role)
